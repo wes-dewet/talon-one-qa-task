41160273e423ea98d4f5da3c4ba77718112a1ba4
Wesley De Wet <wkdewet@gmail.com>
2023-05-31 16:42:08 +0200

    Initial commit

---
+++ tests/run-newman.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+// File destination will be created or overwritten by default.
+fs.copyFile('./db-test-data-snapshot.json', './db.json', (err) => {
+  if (err) throw err;
+  console.log('db.json has been seeded from snapshot');
+});
+
+const newman = require('newman'); // require newman in your project
+
+// call newman.run to pass `options` object and wait for callback
+newman.run(
+  {
+    collection: require('./regression-tests.postman_collection.json'),
+    environment: require('./local.postman_environment.json'),
+    reporters: ['cli'],
+    reporter: {},
+  },
+  (error, summary) => {
+    const err = error || summary.error;
+    if (err) {
+      console.error('Regression Tests collection run failed with error');
+      throw err;
+    }
+    console.log('Collection run completed!');
+    const numberOfFailures = summary.run.failures.length;
+    if (numberOfFailures > 0) {
+      console.log(`${JSON.stringify(numberOfFailures)} failures found. Setting exit code to '1'.`);
+      process.exitCode = 1;
+    }
+  },
+);
